d452262640a4be37fafebde54b0eff91367aa820
Tomer Matmon <tomer.matmon@gmail.com>
2019-06-27 18:35:33 +0300

    Updates

---
+++ precache-manifest.a21ed8643f631c48aefcedb6d796fbf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202af5512d3b26b935186f068dee38ad",
+    "url": "/Tomer51m/Weather-App/index.html"
+  },
+  {
+    "revision": "c5d30ee49f76e70624b5",
+    "url": "/Tomer51m/Weather-App/static/css/main.a0e3361f.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/Tomer51m/Weather-App/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "c5d30ee49f76e70624b5",
+    "url": "/Tomer51m/Weather-App/static/js/main.4d189d72.chunk.js"
+  },
+  {
+    "revision": "241f279e9ffea7a185fb",
+    "url": "/Tomer51m/Weather-App/static/js/runtime~main.c9810de5.js"
+  }
+]);
